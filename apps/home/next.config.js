f3510ffd5e45f65cf137d36e6dbf2bbfd8c50683
Aaron Miller <drkstr101@gmail.com>
2023-05-09 20:00:48 +0000

    feat: add cabbage ui skel

--- apps/home/next.config.js
+++ apps/home/next.config.js
@@ -2,11 +2,20 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
+const { resolve } = require('path');
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  env: {
+    stackbitPreview: process.env["STACKBIT_PREVIEW"] ?? "",
+  },
+  trailingSlash: true,
+  webpack: (config) => {
+    config.watchOptions.ignored.push(resolve('content/pages/**'));
+    return config;
+  },
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
