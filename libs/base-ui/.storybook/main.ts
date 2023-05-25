fb51151e1c6cd7fbdf882c5d55d3ee113066f82d
Aaron Miller <drkstr101@gmail.com>
2023-05-25 22:01:14 +0000

    feat: add tailwindcss

--- libs/base-ui/.storybook/main.ts
+++ libs/base-ui/.storybook/main.ts
@@ -1,8 +1,8 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../src/lib/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-essentials'],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-styling'],
   framework: {
     name: '@storybook/react-vite',
     options: {
