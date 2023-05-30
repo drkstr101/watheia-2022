5bffa97c67ead658a5d33c7cabfcddae2ee910b0
Aaron Miller <drkstr101@gmail.com>
2023-05-30 02:25:09 +0000

    feat: add seo support

--- apps/home/pages/_app.tsx
+++ apps/home/pages/_app.tsx
@@ -1,12 +1,25 @@
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
 import '@watheia/design.theme.styles.global';
+import { DefaultSeo } from 'next-seo';
 import { AppProps } from 'next/app';
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <DefaultSeo
+        titleTemplate="%s | Watheia Labs"
+        defaultTitle="Watheia Labs - We make technology easy"
+        openGraph={{
+          type: 'website',
+          locale: 'en_US',
+          url: 'https://watheia.com/',
+          siteName: 'watheia',
+        }}
+      />
       <Component {...pageProps} />
-      <Analytics />
+      <GoogleAnalytics trackPageViews />
+      <VercelAnalytics />
     </>
   );
 }
