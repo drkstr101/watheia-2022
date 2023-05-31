245411ff3e891fa8b62dcb041729440c094df489
Aaron Miller <drkstr101@gmail.com>
2023-05-31 23:27:04 +0000

    fix: increase font size scale
    Signed-off-by: Aaron Miller <drkstr101@gmail.com>

--- tailwind-workspace-preset.js
+++ tailwind-workspace-preset.js
@@ -4,19 +4,18 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 module.exports = {
   theme: {
     fontSize: {
-      xs: ['0.75rem', { lineHeight: '1rem' }],
-      sm: ['0.875rem', { lineHeight: '1.5rem' }],
-      base: ['1rem', { lineHeight: '1.5rem' }],
-      lg: ['1.125rem', { lineHeight: '2rem' }],
-      xl: ['1.25rem', { lineHeight: '1.75rem' }],
-      '2xl': ['1.5rem', { lineHeight: '2rem' }],
-      '3xl': ['2rem', { lineHeight: '3rem' }],
-      '4xl': ['2.5rem', { lineHeight: '3rem' }],
-      '5xl': ['3rem', { lineHeight: '1' }],
-      '6xl': ['3.75rem', { lineHeight: '1' }],
-      '7xl': ['4.5rem', { lineHeight: '1' }],
-      '8xl': ['6rem', { lineHeight: '1' }],
-      '9xl': ['8rem', { lineHeight: '1' }],
+      xs: ['0.875rem', { lineHeight: '1.5rem' }],
+      sm: ['1rem', { lineHeight: '1.5rem' }],
+      base: ['1.125rem', { lineHeight: '2rem' }],
+      lg: ['1.25rem', { lineHeight: '1.75rem' }],
+      xl: ['1.5rem', { lineHeight: '2rem' }],
+      '2xl': ['2rem', { lineHeight: '3rem' }],
+      '3xl': ['2.5rem', { lineHeight: '3rem' }],
+      '4xl': ['3rem', { lineHeight: '1' }],
+      '5xl': ['3.75rem', { lineHeight: '1' }],
+      '6xl': ['4.5rem', { lineHeight: '1' }],
+      '7xl': ['6rem', { lineHeight: '1' }],
+      '8xl': ['8rem', { lineHeight: '1' }],
     },
     extend: {
       animation: {
