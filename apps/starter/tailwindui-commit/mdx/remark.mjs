67b6486eeca3ddda21535f73e468f98a6ebb5351
Aaron R Miller <amiller@watheia.com>
2023-07-06 11:20:30 -0700

    feat: add tailwindui starters (#12)
    * add tailwindui starters
    
    * add prettier-plugin-tailwindcss
    
    * update framer-motion version

---
+++ apps/starter/tailwindui-commit/mdx/remark.mjs
@@ -0,0 +1,5 @@
+import { mdxAnnotations } from 'mdx-annotations';
+import remarkGfm from 'remark-gfm';
+import remarkUnwrapImages from 'remark-unwrap-images';
+
+export const remarkPlugins = [mdxAnnotations.remark, remarkGfm, remarkUnwrapImages];
